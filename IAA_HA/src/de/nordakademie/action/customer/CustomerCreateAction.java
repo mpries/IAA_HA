4219111ee8ec63acf21ea76d8a8c557ffcf4aacd
matthias <matthias@192.168.0.100>
2013-10-27 14:03:15 +0100

    - Added Struts.xml
    - Create Action Classes and Interfaces

---
+++ IAA_HA/src/de/nordakademie/action/customer/CustomerCreateAction.java
@@ -0,0 +1,21 @@
+package de.nordakademie.action.customer;
+
+import com.opensymphony.xwork2.Action;
+import com.opensymphony.xwork2.ActionSupport;
+
+import de.nordakademie.action.interfaces.ICreateAction;
+
+public class CustomerCreateAction extends ActionSupport implements ICreateAction, Action {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8222097196479424369L;
+
+	@Override
+	public String execute() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ IAA_HA/src/de/nordakademie/action/customer/CustomerDeleteAction.java
@@ -0,0 +1,21 @@
+package de.nordakademie.action.customer;
+
+import com.opensymphony.xwork2.Action;
+import com.opensymphony.xwork2.ActionSupport;
+
+import de.nordakademie.action.interfaces.IDeleteAction;
+
+public class CustomerDeleteAction extends ActionSupport implements IDeleteAction, Action {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3179689939121945974L;
+
+	@Override
+	public String execute() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
